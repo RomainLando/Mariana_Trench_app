b1a2f66c8a89f18ee65670cbf0a10fb79c929c92
RAnderson96 <admin@Admins-MacBook-Pro.local>
2022-12-16 21:45:04 +0000

    Rough layout for zone one

---
+++ client/src/components/Zone1.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Zone1.css'
+
+export default function Zone1() {
+  return (
+    <div className='background1' >
+        <img id='sub' src='./media/submarine.png' ></img>
+        <img id='ripple1' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple2' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple3' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple4' className='ripple' src='./media/underwater_ripple.png'></img>
+        {/* <img id='bubble1' src='./media/bubble3.png'></img> */}
+        <h2 id="zone-title" >Epipalagic Zone - The Sunlight Zone</h2>
+
+
+    </div>
+  )
+}
---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Zone1 from '../components/Zone1'
+
+export default function MainContainer() {
+  return (
+    <div>
+        {/* <p>MainContainer</p> */}
+        <Zone1/>
+    </div>
+  )
+}
