64a7131bd3cb97aabbcba88dd737f7e122714fcc
Romain Landolfini <romain.landolfini@gmail.com>
2022-12-17 18:01:23 +0000

    :sparkles: added fetch 'get' functions for backend data

---
+++ client/src/services/QuestionsServices.js
@@ -0,0 +1,33 @@
+const baseURL = 'http://localhost:9000/api/questions/'
+
+export const getQuestions = () => {
+    return fetch(baseURL)
+    .then(res => res.json())
+}
+
+// export const postQuestions = (payload) => {
+//     return fetch(baseURL, {
+//         method: 'POST',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+// }
+
+
+// export const deleteQuestions = (id) => {
+//     return (fetch(baseURL + id + '/delete', {
+//         method: 'DELETE' 
+//     }))
+    
+// }
+
+
+// export const putQuestions = (payload, id) => {
+//     return fetch(baseURL + id + '/edit' , {
+//         method: 'PUT',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+// }
---
+++ client/src/services/TrenchServices.js
@@ -0,0 +1,33 @@
+const baseURL = 'http://localhost:9000/api/trench/'
+
+export const getTrench = () => {
+    return fetch(baseURL)
+    .then(res => res.json())
+}
+
+// export const postTrench = (payload) => {
+//     return fetch(baseURL, {
+//         method: 'POST',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+// }
+
+
+// export const deleteTrench = (id) => {
+//     return (fetch(baseURL + id + '/delete', {
+//         method: 'DELETE' 
+//     }))
+    
+// }
+
+
+// export const putTrench = (payload, id) => {
+//     return fetch(baseURL + id + '/edit' , {
+//         method: 'PUT',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+// }
