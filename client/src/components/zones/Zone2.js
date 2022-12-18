89750ebfce4939f80b99523840ddb7d9132cc7c5
RAnderson96 <rory.cameron.anderson@gmail.com>
2022-12-18 22:14:47 +0000

    :hammer: :zap: added backgrounds and up navigation

--- client/src/components/zones/Zone2.js
+++ client/src/components/zones/Zone2.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Zone2.css'
 export default function Zone2() {
   return (
-    <div>
-      <h4>Zone2</h4>
-      <Link to ="/zone3" ><button>Down</button></Link>
+    <div className='background2'>
+      <h4>Mesopelagic Zone - The Twilight Zone</h4>
+      <div id='up-down'>
+        <Link to ="/zone1" ><button>Up</button></Link>
+        <Link to ="/zone3" ><button>Down</button></Link>
+
+      </div>
+      <img id='sub' src='./media/submarine.png' ></img>
+        {/* <img id='ripple1' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple2' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple3' className='ripple' src='./media/underwater_ripple.png'></img>
+        <img id='ripple4' className='ripple' src='./media/underwater_ripple.png'></img> */}
     </div>
 
   )
