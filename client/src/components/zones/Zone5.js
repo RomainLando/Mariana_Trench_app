af4ac52fd43292517768d034f78c221dd05596af
EJDC <ewanconnor@gmail.com>
2022-12-19 12:22:18 +0000

    pop-up started in zone 5

--- client/src/components/zones/Zone5.js
+++ client/src/components/zones/Zone5.js
@@ -1,17 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Zone5.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Zone5.css";
+import InfoDisplayPopUp from "../InfoDisplayPopup";
 
 export default function Zone5() {
-  return (
-    <div className='background5'>
-        <h4>Hadalpelagic Zone - The Trenches</h4>
-        <div id='up-down'>
-          <Link to ="/zone4" ><button>Up</button></Link>
-        </div>
+  const [isOpen, setIsOpen] = useState(false);
 
-        <img className='sub' src='./media/submarine.png' ></img>
-    
-    </div>
-  )
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="background5">
+        <h4>Hadalpelagic Zone - The Trenches</h4>
+        <div id="up-down">
+          <Link to="/zone4">
+            <button>Up</button>
+          </Link>
+        </div>
+        <div>
+          <input
+            type="button"
+            value="Click to Open Popup"
+            onClick={togglePopup}
+          />
+          {isOpen && (
+            <InfoDisplayPopUp
+              content={
+                <>
+                  <b>This is where the title will go</b>
+                  <p>
+                    This is where the details will go!
+                  </p>
+                </>
+              }
+              handleClose={togglePopup}
+            />
+          )}
+        </div>
+        <img className="sub" src="./media/submarine.png"></img>
+      </div>
+    </>
+  );
 }
