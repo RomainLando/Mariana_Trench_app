276e10fd79d8fbf7556601257912e05fde2b776a
Gintare <gintare.dhawde@gmail.com>
2022-12-19 16:47:30 +0000

    the state is getting set

---
+++ client/src/components/quiz/QuizList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+const QuizList = ({questions}) => {
+
+
+    // console.log(questions)
+    // const question1 = questions[0]
+    // const questiontest = Object(question1)
+
+    // const questiontest2 = questiontest.options
+    // console.log(questiontest2[0]) 
+    
+    // const questionList = [ ...questiontest2]
+    // console.log(questionList[0])
+    // const parts = ['shoulders', 'knees'];
+    // const lyrics = ['head', ...parts, 'and', 'toes'];
+//  ["head", "shoulders", "knees", "and", "toes"]
+
+    // console.log(questiontest.question)
+
+    
+
+    return (
+        <div>
+            <h3>QUesrtion</h3>
+        </div>
+    );
+}
+
+
+
+export default QuizList;
